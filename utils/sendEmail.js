a7656b528ef131f4c52e3cad83d23553cae1afa9
Dilkhush Kumar <dk5847001@gmail.com>
2026-03-04 08:52:09 +0530

    sendEmail.js update

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,28 +1,27 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587, // 465 ki jagah 587
-  secure: false, // STARTTLS use hoga
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
   auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
 const sendEmail = async ({ email, subject, message }) => {
   try {
     const info = await transporter.sendMail({
-      from: `Ultra Vault <${process.env.GMAIL_USER}>`,
+      from: `"Ultra Vault" <${process.env.SMTP_USER}>`,
       to: email,
       subject: subject,
       html: message,
     });
 
-    console.log("✅ Email sent:", info.messageId);
-    return info;
+    console.log("Email sent:", info.messageId);
   } catch (err) {
-    console.error("❌ Email send error:", err.message);
+    console.error("Email error:", err);
     throw err;
   }
 };
